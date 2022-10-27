95d24811197a13b53c098ee2632adec656e80125
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-27 17:41:50 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
+  attr_accessor :sides
+
   # write code here
+  def initialize(a, b, c)
+    # store sides in array
+    @sides = [a, b, c]
+    # sort from smallest value to largest
+    @sides.sort!
+  end
+
+  def kind
+    if @sides.any? {|sides| sides <= 0} || ((@sides[0] + @sides[1] <= @sides[2]))
+      raise TriangleError
+    elsif @sides.uniq.size == 1
+      :equilateral
+    elsif @sides.uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  class TriangleError < StandardError
+    
+  end
 end
+
